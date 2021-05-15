45a7709e9e9ab52c40338d5866a1a025fb89a24d
truart <trubinart@gmail.com>
2021-05-15 09:21:54 +0300

    hw done

---
+++ basketClass.js
@@ -0,0 +1,43 @@
+class Basket {
+
+    constructor() {
+        this.productList = []
+    }
+
+    // Добавить продукт
+    addProduct() {
+        pass
+    }
+
+    // Удалить продукт
+    deleteProduct() {
+        pass
+    }
+
+    // Получить общую сумму карзины
+    getBasketTotalPrice() {
+        pass
+    }
+
+    // Получить общее кол-во продуктов в карзине
+    getBasketProductCount() {
+        pass
+    }
+
+    // Изменть количество конкретного товара в карзине
+    changeCountOfProduct() {
+
+    }
+
+}
+
+
+class BasketItem {
+    constructor(img, name, text, price) {
+        this.img = img
+        this.name = name
+        this.text = text
+        this.price = price
+    }
+}
+
